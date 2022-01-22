daa689206b9060653a84ed0678a546e3c06a6a67
Sadhukhan <sudipsadhukhan0295@gmail.com>
2022-01-22 19:31:10 +0530

    Fixed import issues

--- app/src/main/java/com/lifewithtech/arabiccalendar/MainActivity.kt
+++ app/src/main/java/com/lifewithtech/arabiccalendar/MainActivity.kt
@@ -2,10 +2,22 @@ package com.lifewithtech.arabiccalendar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import android.widget.Toast
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    var dateTest = Calendar.getInstance()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val tv = findViewById<TextView>(R.id.tv_click)
+        tv.setOnClickListener {
+            com.sodainmind.hijridatepicker.ArabicCalendarDialog(onItemClick = {
+                Toast.makeText(this, it.gregDate, Toast.LENGTH_LONG).show()
+
+            }, dateTest).show(supportFragmentManager, "DIALOG")
+        }
     }
 }
